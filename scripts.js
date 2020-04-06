f700a27224bb463a3e8876d65a0902227bece254
ferueda <fe.rueda@outlook.com>
2020-04-05 23:36:36 -0400

    added new card styles and functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const coursesContainer = document.getElementById('courses-container');
+const coursesNumber = document.getElementById('courses-number');
 
 const courses = [
   {
@@ -10,46 +11,68 @@ const courses = [
     url: 'https://www.udemy.com/course/modern-javascript-from-the-beginning/',
     tags: ['javascript'],
     thumbnail: 'https://img-a.udemycdn.com/course/480x270/1463348_52a4_2.jpg',
+    platformLogo: '/assets/logos/udemy-2.svg',
     rating: 4.7,
     students: 16708,
   },
   {
-    name: 'Modern JavaScript From The Beginning',
+    name: 'The Complete JavaScript Course 2020',
     description:
-      'Learn and build projects with pure JavaScript, no frameworks or libraries',
-    author: 'Brad Traversy',
+      'Master JavaScript  with the most complete course! Projects, challenges, quizzes, JavaScript ES6+, OOP, AJAX, Webpack',
+    author: 'Jonas Schmedtmann',
     platform: 'Udemy',
-    url: 'https://www.udemy.com/course/modern-javascript-from-the-beginning/',
+    url: 'https://www.udemy.com/course/the-complete-javascript-course/',
     tags: ['javascript'],
-    thumbnail: 'https://img-a.udemycdn.com/course/480x270/1463348_52a4_2.jpg',
-    rating: 4.7,
-    students: 16708,
+    thumbnail: 'https://img-a.udemycdn.com/course/240x135/851712_fc61_5.jpg',
+    platformLogo: '/assets/logos/udemy-2.svg',
+    rating: 4.6,
+    students: 67690,
   },
   {
-    name: 'Modern JavaScript From The Beginning',
+    name: 'The Modern JavaScript Bootcamp',
     description:
-      'Learn and build projects with pure JavaScript, no frameworks or libraries',
-    author: 'Brad Traversy',
+      'Learn JavaScript by building real-world apps. Includes 3 real-world projects, 80 programming challenges, and ES6/ES7!',
+    author: 'Andrew Mead',
     platform: 'Udemy',
-    url: 'https://www.udemy.com/course/modern-javascript-from-the-beginning/',
+    url: 'https://www.udemy.com/course/modern-javascript/',
     tags: ['javascript'],
-    thumbnail: 'https://img-a.udemycdn.com/course/480x270/1463348_52a4_2.jpg',
+    thumbnail: 'https://img-a.udemycdn.com/course/240x135/1470810_a8b0.jpg',
+    platformLogo: '/assets/logos/udemy-2.svg',
     rating: 4.7,
-    students: 16708,
+    students: 7991,
   },
   {
-    name: 'Modern JavaScript From The Beginning',
+    name: 'Beginners Javascript!',
     description:
       'Learn and build projects with pure JavaScript, no frameworks or libraries',
-    author: 'Brad Traversy',
-    platform: 'Udemy',
-    url: 'https://www.udemy.com/course/modern-javascript-from-the-beginning/',
+    author: 'Wes Bos',
+    platform: 'wesbos',
+    url: 'https://beginnerjavascript.com/',
     tags: ['javascript'],
-    thumbnail: 'https://img-a.udemycdn.com/course/480x270/1463348_52a4_2.jpg',
-    rating: 4.7,
-    students: 16708,
+    thumbnail: '/assets/course-thumbs/beginner-javascript.jpg',
+    platformLogo: '/assets/logos/wes-bos.png',
+    rating: 4.9,
+    students: 12899,
+  },
+  {
+    name: 'Tyler Mcginnis: React',
+    description: `If you're serious about learning React, there's no better place to do it. Originally launched in 2016.`,
+    author: 'Tyler Mcginnis',
+    platform: 'tylermcginnis.com',
+    url: 'https://tylermcginnis.com/courses/react/',
+    tags: ['react'],
+    thumbnail: '/assets/course-thumbs/mcginnisreact.jpg',
+    platformLogo: '/assets/logos/tylermcginnis.svg',
+    rating: 4.8,
+    students: 0,
   },
 ];
+
+coursesNumber.textContent = courses.length;
+
+const formatNumber = (number) => {
+  return String(number).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, '$1,');
+};
 
 const createCourseCard = (course) => {
   const card = document.createElement('div');
@@ -58,12 +81,20 @@ const createCourseCard = (course) => {
   card.innerHTML = `
     <div class="course-card__title">${course.name}</div>
     <div class="course-card__thumb">
-    <img src="${course.thumbnail}" alt="${course.description}" class="thumb__img" />
+    <img src="${course.thumbnail}" alt="${
+    course.description
+  }" class="thumb__img" />
     </div>
     <div class="course-card__values">
       <div class="values__author">by <strong>${course.author} </strong></div>
-      <div>&#11088; ${course.rating} - ${course.students}</div>
-      <div><img src="/assets/logos/udemy-2.svg" /></div>
+      <div>&#11088; ${course.rating} ${
+    course.students
+      ? `<span class="values__reviews">(${formatNumber(
+          course.students
+        )})</span>`
+      : ''
+  }</div>
+      <div><img src="${course.platformLogo}" style="max-height: 45px"/></div>
       
     </div>
     <div class="course-card__desc">${course.description}</div>
