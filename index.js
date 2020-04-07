290c591c126bf98a880730edee54bb726dedf0ff
ferueda <fe.rueda@outlook.com>
2020-04-07 10:03:27 -0400

    created a file for the front end called view

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const config = require('./utils/config');
 const logger = require('./utils/logger');
 
 app.get('/:tech', async (req, res) => {
-  const api_url = `https://www.udemy.com/api-2.0/courses/?search=${req.params.tech}&category=Development&ordering=relevance`;
+  const api_url = `https://www.udemy.com/api-2.0/courses/?search=${req.params.tech}&category=Development&ordering=relevance&page=1&page_size=20`;
   const fetch_response = await fetch(api_url, {
     method: 'GET',
     headers: {
@@ -14,6 +14,7 @@ app.get('/:tech', async (req, res) => {
     },
   });
   const json = await fetch_response.json();
+  console.log(json.results.length);
   res.json(json);
 });
 
