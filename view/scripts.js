5a6547664e1b8751b47ddc25b459d8c5e6869e6f
ferueda <fe.rueda@outlook.com>
2020-04-12 19:03:49 -0400

    fixed nav select functionality, web title link to home and style

--- view/scripts.js
+++ view/scripts.js
@@ -3,100 +3,13 @@ const techElements = [...document.querySelectorAll('.tech-container')];
 
 let courses = [];
 
-// const courses = [
-//   {
-//     name: 'Modern JavaScript From The Beginning',
-//     description:
-//       'Learn and build projects with pure JavaScript, no frameworks or libraries',
-//     author: 'Brad Traversy',
-//     platform: 'Udemy',
-//     url: 'https://www.udemy.com/course/modern-javascript-from-the-beginning/',
-//     tags: ['javascript', 'react'],
-//     thumbnail: 'https://img-a.udemycdn.com/course/480x270/1463348_52a4_2.jpg',
-//     platformLogo: 'assets/logos/udemy-2.svg',
-//     rating: 4.7,
-//     students: 16708,
-//   },
-//   {
-//     name: 'The Complete JavaScript Course 2020',
-//     description:
-//       'Master JavaScript  with the most complete course! Projects, challenges, quizzes, JavaScript ES6+, OOP, AJAX, Webpack',
-//     author: 'Jonas Schmedtmann',
-//     platform: 'Udemy',
-//     url: 'https://www.udemy.com/course/the-complete-javascript-course/',
-//     tags: ['javascript'],
-//     thumbnail: 'https://img-a.udemycdn.com/course/240x135/851712_fc61_5.jpg',
-//     platformLogo: 'assets/logos/udemy-2.svg',
-//     rating: 4.6,
-//     students: 67690,
-//   },
-//   {
-//     name: 'The Modern JavaScript Bootcamp',
-//     description:
-//       'Learn JavaScript by building real-world apps. Includes 3 real-world projects, 80 programming challenges, and ES6/ES7!',
-//     author: 'Andrew Mead',
-//     platform: 'Udemy',
-//     url: 'https://www.udemy.com/course/modern-javascript/',
-//     tags: ['javascript'],
-//     thumbnail: 'https://img-a.udemycdn.com/course/240x135/1470810_a8b0.jpg',
-//     platformLogo: 'assets/logos/udemy-2.svg',
-//     rating: 4.7,
-//     students: 7991,
-//   },
-//   {
-//     name: 'Beginners Javascript!',
-//     description:
-//       'Learn and build projects with pure JavaScript, no frameworks or libraries',
-//     author: 'Wes Bos',
-//     platform: 'wesbos',
-//     url: 'https://beginnerjavascript.com/',
-//     tags: ['javascript'],
-//     thumbnail: 'assets/course-thumbs/beginner-javascript.jpg',
-//     platformLogo: 'assets/logos/wes-bos.png',
-//     rating: 4.9,
-//     students: 12899,
-//   },
-//   {
-//     name: 'Tyler Mcginnis: React',
-//     description: `If you're serious about learning React, there's no better place to do it. Originally launched in 2016.`,
-//     author: 'Tyler Mcginnis',
-//     platform: 'tylermcginnis.com',
-//     url: 'https://tylermcginnis.com/courses/react/',
-//     tags: ['react'],
-//     thumbnail: 'assets/course-thumbs/mcginnisreact.jpg',
-//     platformLogo: 'assets/logos/tylermcginnis.svg',
-//     rating: 4.8,
-//     students: 0,
-//   },
-// ];
-
 let coursesToShow = [...courses];
 
-// const filterCourses = () => {
-//   const filtered = techElements
-//     .filter((el) => el.classList.contains('tech-container--active'))
-//     .map((el) => el.attributes['data-tech'].value);
-
-//   if (filtered.length === 0) {
-//     coursesToShow = [...courses];
-//   } else {
-//     coursesToShow = courses.filter((course) => {
-//       return filtered.includes(...course.tags);
-//     });
-//   }
-
-//   renderCourseCards(coursesToShow);
-// };
-
-const filterCourses = () => {
-  const filtered = techElements
-    .filter((el) => el.classList.contains('tech-container--active'))
-    .map((el) => el.attributes['data-tech'].value);
-
-  if (filtered.length === 0) {
+const filterCourses = (filter) => {
+  if (!filter) {
     coursesToShow = [...courses];
   } else {
-    coursesToShow = courses.filter((course) => filtered.includes(course.tags));
+    coursesToShow = courses.filter((course) => course.tags.includes(filter));
   }
 
   renderCourseCards(coursesToShow);
@@ -117,12 +30,21 @@ const renderCourseCards = (courses) => {
   });
 };
 
-techElements.forEach((element) =>
+techElements.forEach((element) => {
   element.addEventListener('click', () => {
-    element.classList.toggle('tech-container--active');
-    filterCourses();
-  })
-);
+    if (element.classList.contains('tech-container--active')) {
+      element.classList.remove('tech-container--active');
+      filterCourses(null);
+    } else {
+      element.classList.add('tech-container--active');
+      filterCourses(element.dataset.tech);
+    }
+
+    techElements.forEach((e) => {
+      e !== element ? e.classList.remove('tech-container--active') : false;
+    });
+  });
+});
 
 const formatNumber = (number) => {
   return String(number).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, '$1,');
